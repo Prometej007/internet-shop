1d45eb4ae4247f8e3a35ff2bf52a54524e27f88e
prometej007 <nazar-danilyak@mail.ru>
2018-10-30 22:24:33 +0200

    some fix

--- resource-server/src/main/java/com/web/resourceserver/ResourceServerApplication.java
+++ resource-server/src/main/java/com/web/resourceserver/ResourceServerApplication.java
@@ -2,11 +2,11 @@ package com.web.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -18,9 +18,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableJpaRepositories
 @EnableEurekaClient
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerApplication {
+public class ResourceServerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(new Class<?>[]{ResourceServerApplication.class}, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ResourceServerApplication.class);
+    }
 }
