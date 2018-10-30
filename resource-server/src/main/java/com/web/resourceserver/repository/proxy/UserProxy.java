6971bf7e47cddbdd24373039bea7261bc87924b3
prometej007 <nazar-danilyak@mail.ru>
2018-10-30 20:18:21 +0200

    fix

--- resource-server/src/main/java/com/web/resourceserver/repository/proxy/UserProxy.java
+++ resource-server/src/main/java/com/web/resourceserver/repository/proxy/UserProxy.java
@@ -1,20 +1,14 @@
 package com.web.resourceserver.repository.proxy;
 
 import com.web.resourceserver.config.User;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.Principal;
 
 @FeignClient(name = "auth-service")
 public interface UserProxy {
 
-//    @GetMapping("/users/current")
-//    Principal find(@RequestHeader("authorization") String principal);
     @GetMapping("/auth/users/find")
     User find(@RequestParam String principal);
 
